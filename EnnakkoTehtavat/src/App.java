02d72e1cced9eb2583e6326a5f8f2c9863d70b96
Jogis <ronipihajoki@proton.me>
2025-10-26 18:55:58 +0200

    Harj2

--- EnnakkoTehtavat/src/App.java
+++ EnnakkoTehtavat/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        onkoKuumetta("Ihminen", 37);
     }
 // Harjoitus 1 
 // Tee tulostaKuumerajat-metodi. Kun metodia kutsutaan, metodi tulostaa konsoliin:
@@ -13,6 +13,27 @@ System.out.println("Ihminen 37");
 System.out.println("Koira 39");
 System.out.println("Hevonen 38");
 }
-public static void 
-
+// Harjoitus 2
+// Tee onkoKuumetta-metodi. Metodilla on kaksi parametria:
+// 1.tutkittava kohde: hevonen, koira tai ihminen (String)
+// 2.lämpötila (int)
+// Metodi tutkii, onko kohteella kuumetta ja palauttaa tiedon, onko kuumetta (true tai false). 
+// Eli jos metodille lähetetään tiedot ihminen ja 38, metodi palauttaa true, muussa tapauksessa metodi palauttaa false.
+public static boolean  onkoKuumetta (String kohde, int lampotila)
+{
+    if (kohde.equalsIgnoreCase("Ihminen") && lampotila > 37){
+            return true;
+        } else if (kohde.equalsIgnoreCase("Koira") && lampotila > 39)
+    {
+            return true;
+    } else if (kohde.equalsIgnoreCase("Hevonen") && lampotila > 38)
+    {
+            return true;
+    } else 
+    {
+            return false;
+    }
 }
+}
+
+
