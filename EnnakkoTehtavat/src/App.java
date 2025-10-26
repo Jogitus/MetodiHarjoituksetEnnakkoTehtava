9fee45fad02f4d2236b3df790033c80512c5efa8
Jogis <ronipihajoki@proton.me>
2025-10-26 18:34:03 +0200

    Teht1

--- EnnakkoTehtavat/src/App.java
+++ EnnakkoTehtavat/src/App.java
@@ -2,4 +2,17 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+// Harjoitus 1 
+// Tee tulostaKuumerajat-metodi. Kun metodia kutsutaan, metodi tulostaa konsoliin:
+// Ihminen 37
+// Koira 39
+// Hevonen 38
+public static void tulostaKuumerajat ()
+{
+System.out.println("Ihminen 37");
+System.out.println("Koira 39");
+System.out.println("Hevonen 38");
+}
+public static void 
+
 }
